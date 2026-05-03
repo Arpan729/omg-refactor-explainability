8a21b8c64e352104767d2ae2989878c46c4a7c99
dereklee-vidality <derekl@vidality.com>
2026-05-03 14:12:22 +0800

    updated mp4 to wav script to add testing set

--- src/raw_data/scripts/mp4_to_wav.sh
+++ src/raw_data/scripts/mp4_to_wav.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 VALIDATION_DIRECTORY="../validation/audio";
 TRAINING_DIRECTORY="../training/audio";
+TESTING_DIRECTORY="../testing/audio";
 
 if [ ! -d "$VALIDATION_DIRECTORY" ]; then
     mkdir "$VALIDATION_DIRECTORY"
@@ -12,12 +13,22 @@ if [ ! -d "$TRAINING_DIRECTORY" ]; then
     echo "Training Directory created."
 fi
 
+if [ ! -d "$TESTING_DIRECTORY" ]; then
+    mkdir "$TESTING_DIRECTORY"
+    echo "Testing Directory created."
+fi
+
 for file in ../validation/video/*.mp4; do
     filename=$(basename "$file" .mp4)
-    ffmpeg -i "$file" -vn -ac 1 -ar 16000 -acodec pcm_s16le "../validation/audio/${filename}.mp4.wav"
+    ffmpeg -n -i "$file" -vn -ac 1 -ar 16000 -acodec pcm_s16le "../validation/audio/${filename}.mp4.wav"
 done
 
 for file in ../training/video/*.mp4; do
     filename=$(basename "$file" .mp4)
-    ffmpeg -i "$file" -vn -ac 1 -ar 16000 -acodec pcm_s16le "../training/audio/${filename}.mp4.wav"
+    ffmpeg -n -i "$file" -vn -ac 1 -ar 16000 -acodec pcm_s16le "../training/audio/${filename}.mp4.wav"
+done
+
+for file in ../testing/video/*.mp4; do
+    filename=$(basename "$file" .mp4)
+    ffmpeg -n -i "$file" -vn -ac 1 -ar 16000 -acodec pcm_s16le "../testing/audio/${filename}.mp4.wav"
 done
